84f753d75c37cf33be53196e65c062958528a0b1
Bandell <Bandell@i.ua>
2023-09-10 01:59:59 +0300

    added date

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("Зацепін Богдан ПЗ-20-1/9 ");
+        System.out.println("10.09.2023");
     }
 }
