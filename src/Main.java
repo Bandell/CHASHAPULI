006689c0fc4572f94b39d7f54942569b3e141fe6
Bandell <68690389+Bandell@users.noreply.github.com>
2023-09-10 02:05:50 +0300

    Update Main.java
    add commentary

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Зацепін Богдан ПЗ-20-1/9 ");
         System.out.println("10.09.2023");
+        //this comment just for test!!!
     }
 }
