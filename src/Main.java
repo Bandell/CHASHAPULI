2c02ec0de880fe4e2c4178607ef8c3bb0902ed33
Bandell <Bandell@i.ua>
2023-09-20 13:56:38 +0300

    edit date to current

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("Зацепін Богдан ПЗ-20-1/9 ");
-        System.out.println("10.09.2023");
+        System.out.println(LocalDate.now());
     }
 }
